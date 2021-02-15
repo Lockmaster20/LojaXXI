9b33e142ff5de53817fa158d308373c8c535717d
Lockmaster20 <diogoafernandes20@gmail.com>
2021-02-15 16:50:43 +0000

    Initial Commit

---
+++ db.py
@@ -0,0 +1,7 @@
+class Database:
+
+    def __init__(self):
+        self.Host = 'ec2-54-216-185-51.eu-west-1.compute.amazonaws.com'
+        self.Database = 'd485h4ldp3begi'
+        self.User = 'sjtwupprarsihj'
+        self.Password = '01a5c35b7e4d70e92997a625a89b7a4cb5b656572461f259a46db3b481262c87'
