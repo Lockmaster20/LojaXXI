7733c678f3fe4ea7a90530f36ee381734f84528a
Lockmaster20 <diogoafernandes20@gmail.com>
2021-03-08 14:29:08 +0000

    Tutorial 20

--- artigos.py
+++ artigos.py
@@ -18,6 +18,15 @@ class Artigos:
         self.created = None  # Data de criação
         self.updated = None  # Data de alteração
 
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        db.execute("CREATE TABLE IF NOT EXISTS categorias (id serial primary key, category text)")
+        db.execute("CREATE TABLE IF NOT EXISTS artigos (id serial primary key, category int, brand text,"
+                   "description text, price numeric,reference text, ean text, stock int, created date, updated date,"
+                   "CONSTRAINT fk_category foreign key (category) references categorias(id))")
+        ficheiro.commit()
+        ficheiro.close()
+
 
     def herokudb(self):
         from db import Database
@@ -29,10 +38,20 @@ class Artigos:
     def inserirA(self, category, brand, description, price):
         ficheiro = self.herokudb()
         db = ficheiro.cursor()
-        db.execute("CREATE TABLE IF NOT EXISTS artigos "
-                   "(id serial primary key, category text,brand text, description text, price numeric,"
-                   " reference text, ean text, stock int, created date, updated date)")
-        db.execute("INSERT INTO artigos VALUES (DEFAULT, %s, %s, %s, %s)", (category, brand, description, price,))
+
+        catId = self.existeC(category)
+        if not catId:
+            self.inserirC(category)
+            catId = self.existeC(category)
+        db.execute("INSERT INTO artigos VALUES (DEFAULT, %s, %s, %s, %s)", (catId, brand, description, price,))
+
+        ficheiro.commit()
+        ficheiro.close()
+
+    def inserirC(self, category):
+        ficheiro = self.herokudb()
+        db = ficheiro.cursor()
+        db.execute("INSERT INTO categorias VALUES (DEFAULT ,%s)", (category,))
         ficheiro.commit()
         ficheiro.close()
 
@@ -54,6 +73,17 @@ class Artigos:
             ficheiro = self.herokudb()
             db = ficheiro.cursor()
             db.execute("SELECT * FROM artigos WHERE id = %s", (id,))
+            valor = db.fetchone()
+            ficheiro.close()
+        except:
+            valor = None
+        return valor
+
+    def existeC(self, category):
+        try:
+            ficheiro = self.herokudb()
+            db = ficheiro.cursor()
+            db.execute("SELECT id FROM categorias WHERE category = %s", (category,))
             valor = db.fetchone()
             ficheiro.close()
         except:
@@ -106,6 +136,19 @@ class Artigos:
         except:
             valor = ""
         return valor
+
+    @property
+    def listaC(self):
+        try:
+            ficheiro = self.herokudb()
+            db = ficheiro.cursor()
+            db.execute("select category from categorias")
+            valor = db.fetchall()
+            ficheiro.close()
+        except:
+            valor = ""
+        return valor
+
 
     @property
     def campos(self):
